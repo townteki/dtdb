c30f2d4bd04c727ffae056f761927be2bd3c95d4
justintwynn <justintwynn@gmail.com>
2017-08-02 15:56:02 -0400

    PBE RSS and Discord changes

--- src/Dtdb/BuilderBundle/Resources/public/js/discord.js
+++
@@ -1,9 +0,0 @@
-
-discordWidget.init({
-  serverId: '227387666046386176',
-  title: 'Doomtown: Reloaded Discord',
-  join: true,
-  alphabetical: false,
-  theme: 'light',
-  showAllUsers: false
-});
