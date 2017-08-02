78ba5cea03a056a569814ce33fe62490edff2de9
justintwynn <justintwynn@gmail.com>
2017-08-02 14:10:20 -0400

    Add support for new Discord widget

---
+++ src/Dtdb/BuilderBundle/Resources/public/js/discord.js
@@ -0,0 +1,10 @@
+
+discordWidget.init({
+  serverId: '227387666046386176',
+  title: 'Doomtown: Reloaded Discord',
+  join: true,
+  alphabetical: false,
+  theme: 'light',
+  showAllUsers: false
+});
+discordWidget.render();
